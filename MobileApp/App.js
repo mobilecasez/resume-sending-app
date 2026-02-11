1a8ade7467b3addfed35128b69abc2c73df7ebe8
zsellr <145708334+zsellr@users.noreply.github.com>
2026-02-11 16:05:39 +0530

    Lightened loading overlay background from 75% to 50% opacity

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -5293,7 +5293,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
     alignItems: 'center',
     zIndex: 9999,
